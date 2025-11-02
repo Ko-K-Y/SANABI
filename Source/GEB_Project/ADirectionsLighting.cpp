781e33817a4679127a114fa8a38654835acc7a96
YOUNGHO LEE <youngholee205205@gmail.com>
2025-11-02 21:55:34 +0900

    길찾기 조명

---
+++ Source/GEB_Project/ADirectionsLighting.cpp
@@ -0,0 +1,82 @@
+﻿#include "ADirectionsLighting.h"
+#include "Components/BoxComponent.h"
+#include "Components/PointLightComponent.h"
+#include "GameFramework/Pawn.h"
+
+AADirectionsLighting::AADirectionsLighting()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Trigger (Root)
+	Trigger = CreateDefaultSubobject<UBoxComponent>(TEXT("Trigger"));
+	RootComponent = Trigger;
+	Trigger->SetBoxExtent(FVector(100.f));
+	Trigger->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	Trigger->SetCollisionResponseToAllChannels(ECR_Ignore);
+	Trigger->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	Trigger->SetGenerateOverlapEvents(true);
+
+	// Light
+	PointLight = CreateDefaultSubobject<UPointLightComponent>(TEXT("PointLight"));
+	PointLight->SetupAttachment(RootComponent);
+	PointLight->SetIntensity(0.f);     // 시작은 꺼진 상태
+	PointLight->SetVisibility(false);
+}
+
+void AADirectionsLighting::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Trigger->OnComponentBeginOverlap.AddDynamic(this, &AADirectionsLighting::OnTriggerBeginOverlap);
+	Trigger->OnComponentEndOverlap.AddDynamic(this, &AADirectionsLighting::OnTriggerEndOverlap);
+}
+
+void AADirectionsLighting::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AADirectionsLighting::OnTriggerBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+	bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!OtherActor || OtherActor == this) return;
+
+	if (APawn* Pawn = Cast<APawn>(OtherActor))
+	{
+		if (!bOnlyPlayerControlled || Pawn->IsPlayerControlled())
+		{
+			++PlayerOverlapCount;
+			UpdateLightState();
+		}
+	}
+}
+
+void AADirectionsLighting::OnTriggerEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (!OtherActor || OtherActor == this) return;
+
+	if (APawn* Pawn = Cast<APawn>(OtherActor))
+	{
+		if (!bOnlyPlayerControlled || Pawn->IsPlayerControlled())
+		{
+			PlayerOverlapCount = FMath::Max(0, PlayerOverlapCount - 1);
+			UpdateLightState();
+		}
+	}
+}
+
+void AADirectionsLighting::UpdateLightState()
+{
+	if (PlayerOverlapCount > 0)
+	{
+		PointLight->SetVisibility(true);
+		PointLight->SetIntensity(OnIntensity);
+	}
+	else
+	{
+		PointLight->SetIntensity(0.f);
+		PointLight->SetVisibility(false);
+	}
+}
---
+++ Source/GEB_Project/ADirectionsLighting.h
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ADirectionsLighting.generated.h"
+
+class UBoxComponent;
+class UPointLightComponent;
+
+UCLASS()
+class GEB_PROJECT_API AADirectionsLighting : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AADirectionsLighting();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	// === Components ===
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UBoxComponent* Trigger;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UPointLightComponent* PointLight;
+
+	// === Runtime state ===
+	int32 PlayerOverlapCount = 0;
+
+	// === Settings ===
+	UPROPERTY(EditAnywhere, Category = "Light")
+	float OnIntensity = 5000.f;
+
+	// 플레이어 조종 중인 Pawn만 반응할지 여부
+	UPROPERTY(EditAnywhere, Category = "Light")
+	bool bOnlyPlayerControlled = true;
+
+	// === Handlers ===
+	UFUNCTION()
+	void OnTriggerBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+		bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnTriggerEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	void UpdateLightState();
+};
